b2651806a043a2b6f3f4cb0d06d4ff0b0356ca0a
Nikolay Strizhak <48862178+ziGFriedman@users.noreply.github.com>
2019-03-27 21:15:01 +0500

    Create Finally.py

---
+++ Finally.py
@@ -0,0 +1,16 @@
+'''Проверка на корректное закрытие файла'''
+import time
+
+try:
+    f = open('poem.txt')
+    while True:     # Наш обычный способ читать файлы
+        line = f.readline()
+        if len(line) == 0:
+            break
+        print(line, end='')
+        time.sleep(2)   # Пусть подождёт некоторое время
+except KeyboardInterrupt:
+    print('!!Вы отменили чтение файла.')
+finally:
+    f.close()
+    print('(Очистка: Закрытие файла)')
